bfc50efecc1eccd0a6f168d5ddbf14d24ae7c6b5
chris <chris@838fabe5-b1a8-4b81-9896-7a84386980e9>
2009-08-05 22:48:59 +0000

    Unit tests for the SplitIntList class
    git-svn-id: http://svn.mkgmap.org.uk/splitter/trunk@42 838fabe5-b1a8-4b81-9896-7a84386980e9

---
+++ test/uk/me/parabola/splitter/TestIntList.java
@@ -0,0 +1,38 @@
+/**
+ * Copyright © 2008 Chris Miller
+ */
+package uk.me.parabola.splitter;
+
+import org.testng.annotations.Test;
+import org.testng.Assert;
+
+/**
+ * 
+ */
+public class TestIntList {
+	@Test
+	public void testIntList() {
+		SplitIntList list = new SplitIntList(10);
+		for (int i = 0; i < 100; i++) {
+			list.add(i);
+		}
+
+		Assert.assertEquals(list.size(), 100);
+
+		for (int i = 0; i < 100; i++) {
+			Assert.assertEquals(list.get(i), i);
+		}
+
+		SplitIntList.Iterator it = list.getIterator();
+		int i = 0;
+		while (it.hasNext()) {
+			Assert.assertEquals(it.next(), i++);
+		}
+
+		it = list.getDeletingIterator();
+		i = 0;
+		while (it.hasNext()) {
+			Assert.assertEquals(it.next(), i++);
+		}
+	}
+}
