04758a7b348e25c6e9c91fc2e237659f2efacf76
chris <chris@838fabe5-b1a8-4b81-9896-7a84386980e9>
2009-08-08 11:51:14 +0000

    Broke startElement()/endElement() into various submethods to tidy up the code (and possibly help hotspot?)
    git-svn-id: http://svn.mkgmap.org.uk/splitter/trunk@62 838fabe5-b1a8-4b81-9896-7a84386980e9

--- src/uk/me/parabola/splitter/SplitParser.java
+++ src/uk/me/parabola/splitter/SplitParser.java
@@ -82,93 +82,116 @@ class SplitParser extends AbstractXppParser {
 		String name = getParser().getName();
 		if (mode == 0) {
 			if (name.equals("node")) {
-				mode = MODE_NODE;
-
-				String id = getAttr("id");
-				String slat = getAttr("lat");
-				String slon = getAttr("lon");
-
-				double lat = Double.parseDouble(slat);
-				double lon = Double.parseDouble(slon);
-
-				currentNode = new StringNode(Utils.toMapUnit(lat), Utils.toMapUnit(lon), id, slat, slon);
-				currentNodeAreaSet = 0;
+				startNode();
 			} else if (name.equals("way")) {
-				mode = MODE_WAY;
-				String id = getAttr("id");
-				currentWay = new StringWay(id);
-				currentWayAreaSet.clear();
-
+				startWay();
 			} else if (name.equals("relation")) {
-				mode = MODE_RELATION;
-				String id = getAttr("id");
-				currentRelation = new StringRelation(id);
-				currentRelAreaSet.clear();
+				startRelation();
 			}
 		} else if (mode == MODE_NODE) {
-			if (name.equals("tag")) {
-				currentNode.addTag(getAttr("k"), getAttr("v"));
-			}
+			processNode(name);
 		} else if (mode == MODE_WAY) {
-			if (name.equals("nd")) {
-				String sid = getAttr("ref");
-
-				// Get the list of areas that the node is in.  A node may be in
-				// more than one area because of overlap.
-				int set = coords.get(Integer.parseInt(sid));
-
-				// add the list of areas to the currentWayAreaSet
-				if (set != 0) {
-					int mask = 0xff;
-					for (int slot = 0; slot < 4; slot++, mask <<= 8) {
-						int val = (set & mask) >>> (slot * 8);
-						if (val == 0)
-							break;
-						currentWayAreaSet.set(val - 1);
-					}
-				}
-				currentWay.addRef(sid);
-			} else if (name.equals("tag")) {
-				currentWay.addTag(getAttr("k"), getAttr("v"));
-			}
+			processWay(name);
 		} else if (mode == MODE_RELATION) {
-			if (name.equals("tag")) {
-				currentRelation.addTag(getAttr("k"), getAttr("v"));
-			} else if (name.equals("member")) {
-				String type = getAttr("type");
-				String ref = getAttr("ref");
-				currentRelation.addMember(type, ref, getAttr("role"));
+			processRelation(name);
+		}
+		return false;
+	}
 
-				int iref = Integer.parseInt(ref);
-				int set = 0;
-				long[] bigSet;
-				if ("node".equals(type)) {
-					set = coords.get(iref);
-				} else if ("way".equals(type)) {
-					set = ways.get(iref);
+	private void startNode() {
+		mode = MODE_NODE;
+
+		String id = getAttr("id");
+		String slat = getAttr("lat");
+		String slon = getAttr("lon");
+
+		double lat = Double.parseDouble(slat);
+		double lon = Double.parseDouble(slon);
+
+		currentNode = new StringNode(Utils.toMapUnit(lat), Utils.toMapUnit(lon), id, slat, slon);
+		currentNodeAreaSet = 0;
+	}
+
+	private void startWay() {
+		mode = MODE_WAY;
+		String id = getAttr("id");
+		currentWay = new StringWay(id);
+		currentWayAreaSet.clear();
+	}
+
+	private void startRelation() {
+		mode = MODE_RELATION;
+		String id = getAttr("id");
+		currentRelation = new StringRelation(id);
+		currentRelAreaSet.clear();
+	}
+
+	private void processNode(String name) {
+		if (name.equals("tag")) {
+			currentNode.addTag(getAttr("k"), getAttr("v"));
+		}
+	}
+
+	private void processWay(String name) {
+		if (name.equals("nd")) {
+			String sid = getAttr("ref");
+
+			// Get the list of areas that the node is in.  A node may be in
+			// more than one area because of overlap.
+			int set = coords.get(Integer.parseInt(sid));
+
+			// add the list of areas to the currentWayAreaSet
+			if (set != 0) {
+				int mask = 0xff;
+				for (int slot = 0; slot < 4; slot++, mask <<= 8) {
+					int val = (set & mask) >>> (slot * 8);
+					if (val == 0)
+						break;
+					currentWayAreaSet.set(val - 1);
 				}
-				if (set != 0) {
-					int mask = 0xff;
-					for (int slot = 0; slot < 4; slot++, mask <<= 8) {
-						int val = (set & mask) >>> (slot * 8);
-						if (val == 0)
-							break;
-						// val - 1 because the areas held in 'ways' are in the range 1-255
-						currentRelAreaSet.set(val - 1);
-					}
-				} else if ((bigSet = bigWays.get(iref)) != null) {
-					// Copy bits from bigSet to currentRelAreaSet
-					for (int i = 0; i < bigSet.length; i++) {
-						for (int j = 0; i < 64; j++) {
-							if ((bigSet[i / 64] & (1 << (j % 64))) != 0) {
-								currentRelAreaSet.set(i * 64 + j);
-							}
+			}
+			currentWay.addRef(sid);
+		} else if (name.equals("tag")) {
+			currentWay.addTag(getAttr("k"), getAttr("v"));
+		}
+	}
+
+	private void processRelation(String name) {
+		if (name.equals("tag")) {
+			currentRelation.addTag(getAttr("k"), getAttr("v"));
+		} else if (name.equals("member")) {
+			String type = getAttr("type");
+			String ref = getAttr("ref");
+			currentRelation.addMember(type, ref, getAttr("role"));
+
+			int iref = Integer.parseInt(ref);
+			int set = 0;
+			long[] bigSet;
+			if ("node".equals(type)) {
+				set = coords.get(iref);
+			} else if ("way".equals(type)) {
+				set = ways.get(iref);
+			}
+			if (set != 0) {
+				int mask = 0xff;
+				for (int slot = 0; slot < 4; slot++, mask <<= 8) {
+					int val = (set & mask) >>> (slot * 8);
+					if (val == 0)
+						break;
+					// val - 1 because the areas held in 'ways' are in the range 1-255
+					currentRelAreaSet.set(val - 1);
+				}
+			} else if ((bigSet = bigWays.get(iref)) != null) {
+				// Copy bits from bigSet to currentRelAreaSet
+				for (int i = 0; i < bigSet.length; i++) {
+					for (int j = 0; i < 64; j++) {
+						if ((bigSet[i / 64] & (1 << (j % 64))) != 0) {
+							currentRelAreaSet.set(i * 64 + j);
 						}
 					}
 				}
 			}
 		}
-		return false;
 	}
 
 	/**
@@ -178,60 +201,56 @@ class SplitParser extends AbstractXppParser {
 		String name = getParser().getName();
 		if (mode == MODE_NODE) {
 			if (name.equals("node")) {
-				mode = 0;
-				nodeCount++;
-				try {
-					writeNode();
-				} catch (IOException e) {
-					throw new XmlPullParserException("failed to write node", getParser(), e);
-				}
-				if (nodeCount % NODE_STATUS_UPDATE_THRESHOLD == 0) {
-					System.out.println(Utils.format(nodeCount) + " nodes processed...");
-				}
+				endNode();
 			}
 		} else if (mode == MODE_WAY) {
 			if (name.equals("way")) {
-				mode = 0;
-				wayCount++;
-				try {
-					writeWay();
-				} catch (IOException e) {
-					throw new XmlPullParserException("failed to write way", getParser(), e);
-				}
-				if (wayCount % WAY_STATUS_UPDATE_THRESHOLD == 0) {
-					System.out.println(Utils.format(wayCount) + " ways processed...");
-				}
+				endWay();
 			}
 		} else if (mode == MODE_RELATION) {
 			if (name.equals("relation")) {
-				mode = 0;
-				relationCount++;
-				try {
-					writeRelation();
-				} catch (IOException e) {
-					throw new XmlPullParserException("failed to write relation", getParser(), e);
-				}
-				if (relationCount % RELATION_STATUS_UPDATE_THRESHOLD == 0) {
-					System.out.println(Utils.format(relationCount) + " relations processed...");
-				}
+				endRelation();
 			}
 		}
 	}
 
-	private int addToSet(int set, int v, String desc) {
-		int val = v;
-		for (int mask = 0xff; mask != 0; mask <<= 8) {
-			int setval = set & mask;
-			if (setval == 0) {
-				return set | val;
-			} else if (setval == val) {
-				return set;
-			}
-			val <<= 8;
+	private void endNode() throws XmlPullParserException {
+		mode = 0;
+		nodeCount++;
+		try {
+			writeNode();
+		} catch (IOException e) {
+			throw new XmlPullParserException("failed to write node", getParser(), e);
 		}
-		// it was not added
-		System.err.println(desc + " in too many areas.");
-		return set;
+		if (nodeCount % NODE_STATUS_UPDATE_THRESHOLD == 0) {
+			System.out.println(Utils.format(nodeCount) + " nodes processed...");
+		}
+	}
+
+	private void endWay() throws XmlPullParserException {
+		mode = 0;
+		wayCount++;
+		try {
+			writeWay();
+		} catch (IOException e) {
+			throw new XmlPullParserException("failed to write way", getParser(), e);
+		}
+		if (wayCount % WAY_STATUS_UPDATE_THRESHOLD == 0) {
+			System.out.println(Utils.format(wayCount) + " ways processed...");
+		}
+	}
+
+	private void endRelation() throws XmlPullParserException {
+		mode = 0;
+		relationCount++;
+		try {
+			writeRelation();
+		} catch (IOException e) {
+			throw new XmlPullParserException("failed to write relation", getParser(), e);
+		}
+		if (relationCount % RELATION_STATUS_UPDATE_THRESHOLD == 0) {
+			System.out.println(Utils.format(relationCount) + " relations processed...");
+		}
 	}
 
 	private boolean seenRel;
@@ -280,16 +299,12 @@ class SplitParser extends AbstractXppParser {
 
 	private void writeNode() throws IOException {
 		for (int n = 1; n <= areas.length; n++) {
-			SubArea a = areas[n - 1];
-			boolean found = a.write(currentNode);
+			boolean found = areas[n - 1].write(currentNode);
 			if (found) {
-				if (currentNodeAreaSet == n) {
-					System.out.println("Didn't think this could happen?! " + currentNode.getStringId() + " " + n);
-				} else if (currentNodeAreaSet == 0) {
+				if (currentNodeAreaSet == 0) {
 					currentNodeAreaSet = n;
 				} else {
-					currentNodeAreaSet = addToSet(currentNodeAreaSet, n,
-									"Node " + currentNode.getStringId());
+					currentNodeAreaSet = addToSet(currentNodeAreaSet, n, currentNode.getStringId());
 				}
 			}
 		}
@@ -297,5 +312,21 @@ class SplitParser extends AbstractXppParser {
 		if (currentNodeAreaSet != 0) {
 			coords.put(currentNode.getId(), currentNodeAreaSet);
 		}
+	}
+
+	private int addToSet(int set, int v, String desc) {
+		int val = v;
+		for (int mask = 0xff; mask != 0; mask <<= 8) {
+			int setval = set & mask;
+			if (setval == 0) {
+				return set | val;
+			} else if (setval == val) {
+				return set;
+			}
+			val <<= 8;
+		}
+		// it was not added
+		System.err.println("Node " + desc + " in too many areas.");
+		return set;
 	}
 }
