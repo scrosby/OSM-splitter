5bd428eb6cd5739df845fc83f30a7b25faaf2a3a
chris <chris@838fabe5-b1a8-4b81-9896-7a84386980e9>
2009-09-20 14:55:35 +0000

    Updated copyright headers to make them consistent and GPL3 friendly
    git-svn-id: http://svn.mkgmap.org.uk/splitter/trunk@97 838fabe5-b1a8-4b81-9896-7a84386980e9

--- src/uk/me/parabola/splitter/Main.java
+++ src/uk/me/parabola/splitter/Main.java
@@ -1,19 +1,16 @@
 /*
- * Copyright (C) 2007 Steve Ratcliffe
- * 
- *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License version 3
- *  as published by the Free Software Foundation.
- * 
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- * 
- * 
- * Author: Steve Ratcliffe
- * Create date: Dec 18, 2007
+ * Copyright (c) 2009.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
  */
+
 package uk.me.parabola.splitter;
 
 import java.io.File;
