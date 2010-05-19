00517ac2b0946bfe8ed944a4ce897918495f316f
Scott Crosby <scrosby@cs.rice.edu>
2010-05-18 21:14:52 -0500

    Lazily generate the tag hash table.

--- src/uk/me/parabola/splitter/Element.java
+++ src/uk/me/parabola/splitter/Element.java
@@ -12,6 +12,7 @@
  */
 package uk.me.parabola.splitter;
 
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
@@ -20,7 +21,7 @@ import java.util.Map;
  * @author Steve Ratcliffe
  */
 public class Element {
-	private Map<String, String> tags = new HashMap<String, String>(8);
+	private Map<String, String> tags; 
 	private int id;
 
 	protected void setId(int id) {
@@ -34,11 +35,16 @@ public class Element {
 	public void reset() {
 		this.id = 0;
 		tags.clear();
+		tags = null;
 	}
 
 	public void addTag(String key, String value) {
 		if (key.equals("created_by"))
 			return;
+		// Most elements are nodes. Most nodes have no tags. Create the tag table lazily
+		if (tags == null)
+			tags = new HashMap<String, String>(4);
+
 		tags.put(key, value);
 	}
 
@@ -47,6 +53,8 @@ public class Element {
 	}
 
 	public Iterator<Map.Entry<String, String>> tagsIterator() {
+		if (tags == null)
+			return Collections.EMPTY_SET.iterator();
 		return tags.entrySet().iterator();
 	}
 }
