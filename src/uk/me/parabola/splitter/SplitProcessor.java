b862056519c9f8471c7a3c5404707c85804a1e78
Scott Crosby <scrosby@cs.rice.edu>
2010-09-15 14:23:35 -0500

    New threading code.
    Bundle messages up into batches before enqueuing them.

--- src/uk/me/parabola/splitter/SplitProcessor.java
+++ src/uk/me/parabola/splitter/SplitProcessor.java
@@ -29,9 +29,10 @@ class SplitProcessor implements MapProcessor {
 	private final IntObjMap<long[]> bigWays = new IntObjMap<long[]>();
 
 	private final OSMWriter[] writers;
-	private final BlockingQueue<Element>[] writerInputQueues;
-	private final BlockingQueue<InputQueueInfo> writerInputQueue;
+	private final InputQueueInfo[] writerInputQueues;
+	private final BlockingQueue<InputQueueInfo> toProcess;
 	private final ArrayList<Thread> workerThreads;
+	private final InputQueueInfo STOP_MSG = new InputQueueInfo(null);
 
 	private Node currentNode = new Node();
 	private int currentNodeAreaSet;
@@ -47,13 +48,11 @@ class SplitProcessor implements MapProcessor {
 	SplitProcessor(OSMWriter[] writers, int maxThreads) {
 		this.writers = writers;
 		this.maxThreads = maxThreads;
-		this.writerInputQueue = new ArrayBlockingQueue<InputQueueInfo>(writers.length); 
-		this.writerInputQueues = new BlockingQueue[writers.length];
+		this.toProcess = new ArrayBlockingQueue<InputQueueInfo>(writers.length); 
+		this.writerInputQueues = new InputQueueInfo[writers.length];
 		for (int i = 0; i < writerInputQueues.length;i++) {
-			writerInputQueues[i] = new ArrayBlockingQueue<Element>(NO_ELEMENTS);
-			writerInputQueue.add(new InputQueueInfo(this.writers[i], writerInputQueues[i]));
+			writerInputQueues[i] = new InputQueueInfo(this.writers[i]);
 		}
-
 		currentWayAreaSet = new BitSet(writers.length);
 		currentRelAreaSet = new BitSet(writers.length);
 		
@@ -208,11 +207,18 @@ class SplitProcessor implements MapProcessor {
 	public void endMap() {
 		for (int i = 0; i < writerInputQueues.length; i++) {
 			try {
-				writerInputQueues[i].put(STOP_ELEMENT);
+				writerInputQueues[i].stop();
 			} catch (InterruptedException e) {
 				throw new RuntimeException("Failed to add the stop element for worker thread " + i, e);
 			}
 		}
+		try {
+			toProcess.put(STOP_MSG);// Magic flag used to indicate that all data is done.
+
+		} catch (InterruptedException e1) {
+			e1.printStackTrace();
+		} 
+		
 		for (Thread workerThread : workerThreads) {
 			try {
 				workerThread.join();
@@ -323,23 +329,39 @@ class SplitProcessor implements MapProcessor {
 		try {
 			writerInputQueues[writerNumber].put(element);
 		} catch (InterruptedException e) {
-			throw new RuntimeException("Failed to write node " + element.getId() + " to worker thread " + writerNumber, e);
+			//throw new RuntimeException("Failed to write node " + element.getId() + " to worker thread " + writerNumber, e);
 		}
 	}
 
-	private static class InputQueueInfo {
+	private class InputQueueInfo {
 		private final OSMWriter writer;
-		private final BlockingQueue<Element> inputQueue;
+		private ArrayList<Element> staging;
+		private final BlockingQueue<ArrayList<Element>> inputQueue;
 
-		public InputQueueInfo(OSMWriter writer, BlockingQueue<Element> inputQueue) {
-      this.writer = writer;
-			this.inputQueue = inputQueue;
+		public InputQueueInfo(OSMWriter writer) {
+			inputQueue =  new ArrayBlockingQueue<ArrayList<Element>>(NO_ELEMENTS);
+			this.writer = writer;
+			this.staging = new ArrayList<Element>(STAGING_SIZE);
+		}
+		void put(Element e) throws InterruptedException {
+			staging.add(e);
+			if (staging.size() < STAGING_SIZE) 
+				return;
+			flush();
+		}
+		void flush() throws InterruptedException {
+			//System.out.println("Flush");
+			inputQueue.put(staging);
+			staging = new ArrayList<Element>(STAGING_SIZE);
+			toProcess.put(this);
+		}
+		void stop() throws InterruptedException {
+			flush();
 		}
 	}
 
-	private static final Element STOP_ELEMENT = new Element();
-
-	public static final int NO_ELEMENTS = 1000;
+	public static final int NO_ELEMENTS = 3;
+	final int STAGING_SIZE = 300;
 
 	private class OSMWriterWorker implements Runnable {
 
@@ -357,40 +379,36 @@ class SplitProcessor implements MapProcessor {
 		public void run() {
 			boolean finished = false;
 			while (!finished) {
-				InputQueueInfo workPackage = writerInputQueue.poll();
-				if (workPackage==null) {
+				InputQueueInfo workPackage = null;
+				try {
+					workPackage = toProcess.take();
+				} catch (InterruptedException e1) {
+					e1.printStackTrace();
+					continue;
+				}
+				if (workPackage==STOP_MSG) {
+					try {
+						toProcess.put(STOP_MSG); // Re-inject it so that other threads know that we're exiting.
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
 					finished=true;
 				} else {
+					synchronized (workPackage) {
 					while (!workPackage.inputQueue.isEmpty()) {
-						Element element =null;
+						ArrayList<Element> elements =null;
 						try {
-							element = workPackage.inputQueue.poll();
-							if (element == null) {
-								writerInputQueue.put(workPackage);
-								workPackage=null;
-								break;
-							} else if (element == STOP_ELEMENT) {
-								workPackage=null;
-								System.out.println("Thread " + Thread.currentThread().getName() + " has finished");
-								// this writer is finished
-								break;
-							} else {
+							elements = workPackage.inputQueue.poll();
+							for (Element element : elements ) {
 								processElement(element, workPackage.writer);
 							}
 							
-						} catch (InterruptedException e) {
-							throw new RuntimeException("Thread " + Thread.currentThread().getName() + " failed to get next element", e);
 						} catch (IOException e) {
-							throw new RuntimeException("Thread " + Thread.currentThread().getName() + " failed to write element " + element.getId() + '(' + element.getClass().getSimpleName() + ')', e);
+							throw new RuntimeException("Thread " + Thread.currentThread().getName() + " failed to write element ", e);
 						}
 					}
-					if (workPackage != null) {
-						try {
-							writerInputQueue.put(workPackage);
-						} catch (InterruptedException e) {
-							throw new RuntimeException("Thread " + Thread.currentThread().getName() + " failed to return work package", e);
-						}
 					}
+
 				}
 			}
 		}
